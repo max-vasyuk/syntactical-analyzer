7cf806d4ad8b74f8c07ec06548c2107ca764a78a
artyomnaz <1998artyomnaz@mail.ru>
2019-07-09 14:17:38 +0500

    main.cpp has been modified

--- Syntax_Analys/Syntax_Analys/main.cpp
+++ Syntax_Analys/Syntax_Analys/main.cpp
@@ -4,26 +4,22 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	string input_text = "";			    // весь считанный из файла текст
-	string line = "";					// для считывания одной строки
+	string input_text = "";			// весь считанный из файла текст
 	char result[10] = "ACCEPT";		// массив символов примет в итоге одно из значений (ACCEPT или REJECT)
 	unsigned int buf_length = 0;	// длина строки
 
 	// считываем входные данные из файла "INPUT.txt"
 	fstream file_input;
-	file_input.open("INPUT.txt", std::ios::in);
+	file_input.open("INPUT.txt", ios::in);
 
 	// если файл не был открыт, завершаем работу программы с ошибкой
 	if (!file_input.is_open()) {
-		file_input.close();
 		cout << "Ошибка. Файл \"INPUT.txt\" не найден," << endl;
 		return -1;
 	}
 	
 	// считываем из файла исходные данные
-	while (getline(file_input, line)) {
-		input_text = input_text + line + '\n';
-	}
+	getline(file_input, input_text);
 
 	// закрываем файл
 	file_input.close();
@@ -32,10 +28,10 @@ int main()
 	buf_length = input_text.size();
 
 	// предварительный вывод строки в консоль
-	cout << "содержимое файла:\n" << input_text << "\n";
+	cout << "Cодержимое файла:\n" << input_text << "\n\n";
 
 	// блок транслитерации
-	cout << "блок транслитерации:\n" << func_Translit(input_text, buf_length) << "\n\n";
+	cout << "Блок транслитерации:\n" << func_Translit(input_text, buf_length) << "\n\n";
 
 	// лексический блок
 	func_Leks();
@@ -47,15 +43,15 @@ int main()
 	func_Syntax();
 
 	// открываем файл "OUTPUT.txt" для записи результата синтаксического анализа строки (ACCEPT или RELECT)
-	file_input.open("OUTPUT.txt", std::ios::out);
+	file_input.open("OUTPUT.txt", ios::out);
 
 	// если файл не был открыт, завершаем работу программы с ошибкой
 	if (!file_input.is_open()) {
-		file_input.close();
 		cout << "Ошибка. Файл \"OUTPUT.txt\" не найден," << endl;
 		return -1;
 	}
-	// записали результат работы в файл "OUTPUT.txt"
+
+	// записали результат работы в файл "OUTPUT.txt" и закрыли его
 	file_input.write(result, sizeof(result));
 	file_input.close();
 	return 0;
