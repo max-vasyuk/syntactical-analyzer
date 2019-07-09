d271bf6b53e617170f851731187a672583b21b78
artyomnaz <1998artyomnaz@mail.ru>
2019-07-09 14:02:51 +0500

    Code of client has been modified

--- Syntax_Analys/Syntax_Analys/1_func_Translit.cpp
+++ Syntax_Analys/Syntax_Analys/1_func_Translit.cpp
@@ -1,58 +1,35 @@
 #include "header.h"
-using namespace std;
 
 // func_Translit - блок транслитерации
 //__________________________________________________________________
-// ВХОД:  char *bufer - символьная строка из файла
-//		  buf_length  - длина строки из файла (кол-во символов в ней)
+// ВХОД:  string buffer - символьная строка из файла
+//		  int buf_length  - длина строки из файла (кол-во символов в ней)
 //__________________________________________________________________
-// ВЫХОД: string checker - итоговый набор лексемм
+// ВЫХОД: string checker - итоговый набор лексем
 //__________________________________________________________________
-string func_Translit(char *bufer, unsigned int buf_length)
+string func_Translit(string buffer, int buf_length)
 {
 	string checker = "";	// строка для хранения результатов работы функции
-	// массив цифр
-	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	// массив латинских букв
-	char letters[52] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-						 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-	bool flag;	// проверочный флаг для принятия решений
-
-	// основной цикл по кол-ву символов в строке bufer
-	// последовательно сверяем его с необходимыми символами
-	// и присваиваем соответствующую лексемму
+	// цикл по кол-ву символов в строке buffer
+	// последовательно сверяем его с символами
+	// и присваиваем соответствующую лексему
 	// если совпадений не было, присваиваем символу
 	// статус "ОШИБКА"
 	for (int i = 0; i < buf_length; i++){
-		flag = false;
-		if (bufer[i] == ' '){
-			checker.append("ПРОБЕЛ ");
-			continue;
+		switch (buffer[i])
+		{
+			case ' ': checker.append("ПРОБЕЛ "); break;
+			case ';': checker.append("ТЧКЗПТ "); break;
+			case '=': checker.append("РАВНО "); break;
+			case '\n': checker.append("НВСТРК "); break;
+			default:
+				if (buffer[i] >= 'A' && buffer[i] <= 'Z' || buffer[i] >= 'a' && buffer[i] <= 'z')
+					checker.append("БУКВА ");
+				else if (buffer[i] >= '0' && buffer[i] <= '9')
+					checker.append("ЦИФРА ");
+				else 
+					checker.append("ОШИБКА ");
 		}
-		if (bufer[i] == ';'){
-			checker.append("ТЧКЗПТ ");
-			continue;
-		}
-		if (bufer[i] == '='){
-			checker.append("РАВНО ");
-			continue;
-		}
-		for (unsigned int num_index = 0; num_index < 10; num_index++){
-			if (bufer[i] == numbers[num_index]){
-				checker.append("ЦИФРА ");
-				flag = true;
-				break;
-			}
-		}
-		if (flag) continue;
-		for (unsigned int let_index = 0; let_index < 52; let_index++){
-			if (bufer[i] == letters[let_index]){
-				checker.append("БУКВА ");
-				flag = true;
-				break;
-			}
-		}
-		if (!flag) checker.append("ОШИБКА ");
 	}
 	return checker;	// возвращаем полученные лексеммы далее в работу
 }
--- Syntax_Analys/Syntax_Analys/main.cpp
+++ Syntax_Analys/Syntax_Analys/main.cpp
@@ -1,59 +1,62 @@
 #include "header.h"
-using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	char bufer[255] = "";			// буфер для хранения считанной из файла строки
-	char result[10] = "ACCEPT";		// массив символов примет в итоге одно из значений (ACCEPT или RELECT)
+	string input_text = "";			    // весь считанный из файла текст
+	string line = "";					// для считывания одной строки
+	char result[10] = "ACCEPT";		// массив символов примет в итоге одно из значений (ACCEPT или REJECT)
 	unsigned int buf_length = 0;	// длина строки
 
 	// считываем входные данные из файла "INPUT.txt"
-	fstream FileInput;
-	FileInput.open("INPUT.txt", std::ios::in);
+	fstream file_input;
+	file_input.open("INPUT.txt", std::ios::in);
 
 	// если файл не был открыт, завершаем работу программы с ошибкой
-	if (!FileInput.is_open()){
-		FileInput.close();
+	if (!file_input.is_open()) {
+		file_input.close();
 		cout << "Ошибка. Файл \"INPUT.txt\" не найден," << endl;
 		return -1;
 	}
-		// считываем из файла исходные данные
-		FileInput.read(bufer, 255);
+	
+	// считываем из файла исходные данные
+	while (getline(file_input, line)) {
+		input_text = input_text + line + '\n';
+	}
 
-		// закрываем файл
-		FileInput.close();
+	// закрываем файл
+	file_input.close();
 
-		// определяем длину строки
-		buf_length = strlen(bufer);
+	// определяем длину строки
+	buf_length = input_text.size();
 
-		// предварительный вывод строки в консоль
-		cout << "содержимое файла:" << endl << bufer << endl << endl;
+	// предварительный вывод строки в консоль
+	cout << "содержимое файла:\n" << input_text << "\n";
 
-		// блок транслитерации
-		cout << "блок транслитерации:" << endl << func_Translit(bufer, buf_length) << endl << endl;
+	// блок транслитерации
+	cout << "блок транслитерации:\n" << func_Translit(input_text, buf_length) << "\n\n";
 
-		// лексический блок
-		func_Leks();
+	// лексический блок
+	func_Leks();
 
-		// блок идентификации ключевых слов
-		func_KeyWordIdent();
+	// блок идентификации ключевых слов
+	func_KeyWordIdent();
 
-		// синтаксический блок
-		func_Syntax();
+	// синтаксический блок
+	func_Syntax();
 
-		// открываем файл "OUTPUT.txt" для записи результата синтаксического анализа строки (ACCEPT или RELECT)
-		FileInput.open("OUTPUT.txt", std::ios::out);
+	// открываем файл "OUTPUT.txt" для записи результата синтаксического анализа строки (ACCEPT или RELECT)
+	file_input.open("OUTPUT.txt", std::ios::out);
 
-		// если файл не был открыт, завершаем работу программы с ошибкой
-		if (!FileInput.is_open()){
-			FileInput.close();
-			cout << "Ошибка. Файл \"OUTPUT.txt\" не найден," << endl;
-			return -1;
-		}
-		// записали результат работы в файл "OUTPUT.txt"
-		FileInput.write(result, 10);
-		FileInput.close();
+	// если файл не был открыт, завершаем работу программы с ошибкой
+	if (!file_input.is_open()) {
+		file_input.close();
+		cout << "Ошибка. Файл \"OUTPUT.txt\" не найден," << endl;
+		return -1;
+	}
+	// записали результат работы в файл "OUTPUT.txt"
+	file_input.write(result, sizeof(result));
+	file_input.close();
 	return 0;
 }
