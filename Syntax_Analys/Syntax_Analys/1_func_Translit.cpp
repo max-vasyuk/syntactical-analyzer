630012c2c69aa4f58c9803021fd82e0cb4888ed7
artyomnaz <1998artyomnaz@mail.ru>
2019-07-10 18:19:36 +0500

    New state has been added

--- Syntax_Analys/Syntax_Analys/1_func_Translit.cpp
+++ Syntax_Analys/Syntax_Analys/1_func_Translit.cpp
@@ -6,8 +6,7 @@
 //__________________________________________________________________
 // ВЫХОД: string checker - итоговый набор лексем
 //__________________________________________________________________
-string func_Translit(string buffer)
-{
+string func_Translit(string buffer) {
 	string checker = "";	// строка для хранения результатов работы функции
 	// цикл по кол-ву символов в строке buffer
 	// последовательно сверяем его с символами
@@ -17,24 +16,25 @@ string func_Translit(string buffer)
 	for (int i = 0; i < buffer.size(); i++) {
 		string cur_string = "(";
 		cur_string.push_back(buffer[i]);
-		switch (buffer[i])
-		{
-		case ' ': cur_string.append(",ПРОБЕЛ)"); break;
-		case ';': cur_string.append(",ТЧКЗПТ)"); break;
-		case ':': cur_string.append(",ДВОЕТОЧИЕ)"); break;
-		case '=': cur_string.append(",РАВНО)"); break;
-		case '(': cur_string.append(",ОСКОБКА)"); break;
-		case ')': cur_string.append(",ЗСКОБКА)"); break;
-		default:
-			if (buffer[i] >= 'A' && buffer[i] <= 'Z' || buffer[i] >= 'a' && buffer[i] <= 'z')
-				cur_string.append(",БУКВА)");
-			else if (buffer[i] >= '0' && buffer[i] <= '9')
-				cur_string.append(",ЦИФРА)");
-			else cur_string.append(",ОШИБКА)");
+		switch (buffer[i]) {
+			case ' ': cur_string.append(",ПРОБЕЛ)"); break;
+			case ';': cur_string.append(",ТЧКЗПТ)"); break;
+			case ':': cur_string.append(",ДВОЕТОЧИЕ)"); break;
+			case '=': cur_string.append(",РАВНО)"); break;
+			case '(': cur_string.append(",ОСКОБКА)"); break;
+			case ')': cur_string.append(",ЗСКОБКА)"); break;
+			case '_': cur_string.append(",ПОДЧЕРК)"); break;
+			case '-': case '+': cur_string.append(",ЗНАК)"); break;
+			default:
+				if (buffer[i] >= 'A' && buffer[i] <= 'Z' || buffer[i] >= 'a' && buffer[i] <= 'z')
+					cur_string.append(",БУКВА)");
+				else if (buffer[i] >= '0' && buffer[i] <= '9')
+					cur_string.append(",ЦИФРА)");
+				else cur_string.append(",ОШИБКА)");
 		}
 		if (i != buffer.size() - 1)
 			cur_string.append(", ");
 		checker.append(cur_string);
 	}
-	return checker;	// возвращаем полученные лексеммы далее в работу
+	return checker;	// возвращаем полученные лексемы далее в работу
 }
